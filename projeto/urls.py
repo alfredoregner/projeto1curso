63818406984c3c2de9720cc02301e4a248fdcf04
Alfredo Regner <alfredoregner3@gmail.com>
2025-07-31 21:44:33 -0300

    Configurado para receber imagens na estrutura das listagens

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
+from django.conf import settings            # Importe settings e statis para servir arquivos de mídia no desenvolvimento
+from django.conf.urls.static import static  # Importe settings e statis para servir arquivos de mídia no desenvolvimento
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipes/', include('recipes.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recipes/models.py
+++ recipes/models.py
@@ -6,7 +6,13 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.TextField()
     instructions = models.TextField()
-    cooking_time = models.IntegerField(help_text="Em minutos")
+    cooking_time = models.IntegerField(help_text="Tempo em minutos")
+    image = models.ImageField(upload_to='recipes/', blank=True, null=True) #upload_to='recipes/' significa que as imagens serão salvas em MEDIA_TOOR/recipes
+
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        #Define o nome da tabela no plural para o painel de administração
+        verbose_name_plural = "Recipes"
