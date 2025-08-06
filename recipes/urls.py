9e016770732911e0c2c1da27161df8ac875ff5b7
Alfredo Regner <alfredoregner3@gmail.com>
2025-08-05 21:46:50 -0300

    Aula 05-08

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ from . import views             # Ter acesso a todas as funções e classes no a
 
 urlpatterns = [                 # Onde coloca todas as definições dos caminhos
     #path('', views.home, name='recipes_home'), # View para dar home
-    path('', views.recipe_list, name='recipe_list'), # Nova view para listar
+    path('', views.recipe_list, name='recipe_list'), # Nova view para listar - Define a URL e nomeia ela como 'recipe_detail'
+    path('<int:pk>/', views.recipe_detail, name='recipe_detail'), # Outras URLs do seu aplicativo
 ]
